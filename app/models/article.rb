31268e6b094de33b64a631ee72c26464d3c16b69
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2023-11-08 02:19:40 -0600

    created category model, validate it, update the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
+  include Visible
+  belongs_to :category
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_06_210120) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_080132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,27 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_06_210120) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "public"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "articles", "categories"
+  add_foreign_key "comments", "articles"
 end
